609b271f2ae39b4d6eebeed9c3f08eac229b285d
namara-pipeline <example@example.com>
2024-05-26 20:40:31 +0900

    1st

---
+++ App.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace MicLevelViewer
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        private System.Windows.Forms.ContextMenuStrip _menu;
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private MainWindow _win = null;  // 多重起動抑止用
+
+        // 常駐開始処理
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var icon = GetResourceStream(new Uri("images/level_max.ico", UriKind.Relative)).Stream;
+            _menu = CreateMenu();
+
+            // 通知領域にアイコン
+            _notifyIcon = new System.Windows.Forms.NotifyIcon {
+                Visible = true,
+                Icon = new System.Drawing.Icon(icon),
+                Text = Assembly.GetExecutingAssembly().GetName().Name,
+                ContextMenuStrip = _menu
+            };
+            // アイコン左クリックでMainWindowを表示
+            _notifyIcon.MouseClick += (sender, evt) => {
+                if (evt.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    ShowMainWindow();
+                }
+            };
+            ShowMainWindow();
+
+            // 別タスクで監視処理を実行
+            Task.Run(() => {
+
+
+            });
+        }
+
+        // 常駐終了処理
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _menu.Dispose();
+            _notifyIcon.Dispose();
+
+            base.OnExit(e);
+        }
+
+        // MainWindowを表示
+        private void ShowMainWindow()
+        {
+            if (_win == null)
+            {
+                _win = new MainWindow();
+                _win.ImageSelect += MainWindow_ImageSelect;
+                // マイクの一覧を取得し、MainWindow の ComboBox に追加する
+                _win.PopulateMicrophoneComboBox();
+
+                // 画面中央に表示
+                _win.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                // _win.Show();
+
+                // 閉じるボタンの処理
+                _win.Closing += (sender, e) => {
+                    // 閉じないで非表示にする
+                    _win.Hide();
+                    e.Cancel = true;
+                };
+            }
+            else
+            {
+                // 非表示から表示に変更
+                _win.Show();
+            }
+        }
+
+        private void MainWindow_ImageSelect(object sender, string iconName)
+        {
+            // iconName に基づいてアイコンを設定する
+            string uriString = "images/" + iconName;
+            // システムトレイのアイコン変更
+            var iconSystemTray = GetResourceStream(new Uri(uriString, UriKind.Relative)).Stream;
+            _notifyIcon.Icon = new System.Drawing.Icon(iconSystemTray);
+            // タスクバーのアイコン変更
+            var iconTaskBar = GetResourceStream(new Uri(uriString, UriKind.Relative)).Stream;
+            _win.Icon = BitmapFrame.Create(iconTaskBar);
+        }
+
+        // コンテキストメニュー
+        private ContextMenuStrip CreateMenu()
+        {
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            menu.Items.Add("Setting", null, (sender, e) => { ShowMainWindow(); });
+            menu.Items.Add("Exit", null, (sender, e) => { Shutdown(); });
+            return menu;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Reflection;
+using NAudio.Wave;
+
+
+namespace MicLevelViewer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public event EventHandler<string> ImageSelect;
+        private WaveInEvent waveIn;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = Assembly.GetExecutingAssembly().GetName().Name;
+        }
+
+        // マイクの一覧を取得し、ComboBox に追加する
+        public void PopulateMicrophoneComboBox()
+        {
+            List<string> microphoneList = new List<string>();
+
+            for (var i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                MicrophoneComboBox.Items.Add(WaveIn.GetCapabilities(i).ProductName);
+            }
+
+            if (microphoneList.Count > 0)
+            {
+                // マイクが存在する場合は、ComboBox に追加する
+                foreach (string microphone in microphoneList)
+                {
+                    MicrophoneComboBox.Items.Add(microphone);
+                }
+            }
+            else
+            {
+                MicrophoneComboBox.Items.Add("-");
+            }
+            // 最初のアイテムを選択状態にする
+            MicrophoneComboBox.SelectedIndex = 0;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (waveIn != null)
+            {
+                waveIn.StopRecording();
+                waveIn.DataAvailable -= OnDataAvailable;
+                waveIn.Dispose();
+            }
+
+            waveIn = new WaveInEvent { DeviceNumber = MicrophoneComboBox.SelectedIndex };
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.BufferMilliseconds = 100; // OnDataAvailableが呼ばれる周期 100ms
+            waveIn.StartRecording();
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            var max = 0f;
+            for (var i = 0; i < e.BytesRecorded; i += 2)
+            {
+                var sample = (short)((e.Buffer[i + 1] << 8) | e.Buffer[i + 0]);
+                var sample32 = sample / 32768f;
+                if (sample32 < 0) sample32 = -sample32;
+                if (sample32 > max) max = sample32;
+            }
+            MovingAverage movingAverage = new MovingAverage(10);
+            movingAverage.AddValue(max);
+            double ave = movingAverage.GetAverage();
+
+            Dispatcher.Invoke(() =>
+            {
+                // Console.WriteLine($"{ave:F3}"); // 出力: 0.123
+                // プログレスバー更新
+                textBlock_Vol.Text = (100 * ave).ToString("0.0");
+                progressBar.Value = 100 * ave;
+
+                // アイコン更新
+                string iconName = "level_min.ico";
+                if (ave > 0.80)
+                {
+                    iconName = "level_max.ico";
+                }
+                else if (ave >= 0.050)
+                {
+                    iconName = "level_8.ico";
+                }
+                else if (ave >= 0.030)
+                {
+                    iconName = "level_5.ico";
+                }
+                else if (ave >= 0.010)
+                {
+                    iconName = "level_3.ico";
+                }
+                else if (ave >  0.001)
+                {
+                    iconName = "level_1.ico";
+                }
+                ImageSelect?.Invoke(this, iconName);
+            });
+        }
+    }
+
+    public class MovingAverage
+    {
+        private readonly Queue<double> _values;
+        private readonly int _windowSize;
+        private double _sum;
+
+        public MovingAverage(int windowSize)
+        {
+            _windowSize = windowSize;
+            _values = new Queue<double>(windowSize);
+            _sum = 0;
+        }
+
+        public void AddValue(double val)
+        {
+            if (_values.Count == _windowSize)
+            {
+                _sum -= _values.Dequeue();
+            }
+
+            _values.Enqueue(val);
+            _sum += val;
+        }
+
+        public double GetAverage()
+        {
+            if (_values.Count == 0)
+            {
+                return 0;
+            }
+
+            return _sum / _values.Count;
+        }
+    }
+}
